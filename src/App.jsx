8b8561a295691d0ea14b77359cd742ce611fb216
Antonio <krixen.dev@gmail.com>
2023-11-28 20:03:13 -0300

    agrega estructuración y lógica de la lista de tareas
    la app puede agregar una tarea, marcarla como leída y también como completada

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
+import './App.css';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    console.log('Tasks updated:', tasks);
+  }, [tasks]);
+
+  const handleCompleteTask = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const handleDeleteTask = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
+  const handleAddTask = (newTask) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
 
   return (
-    <>
+    <div>
+      <h1>Task Manager</h1>
+      <TaskForm onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onCompleteTask={handleCompleteTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
